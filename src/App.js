a3a796848ebae7a284d9810283ae377911f35d84
Hiwot-Beshe <beshehiwot@gmail.com>
2021-12-15 14:32:01 -0800

    feat(share message):custmized message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 import TextEditer from "./Components/TextEditer";
+import { useState } from "react";
 
 function App() {
+	const query = new URLSearchParams(window.location.search);
+	const [email, setEmail] = useState("");
+	const [importedGreeting, setImportedGreeting] = useState(
+		query.get("greeting") || "Dear Mom,"
+	);
+	const [importedBody, setImportedBody] = useState(
+		query.get("body") || "Have a comforting and joyous holiday!"
+	);
+	const [importedClosing, setImportedClosing] = useState(
+		query.get("closing") || "Love, your son Juan"
+	);
+	const [customize, setCustomize] = useState(false);
+	const [preview, setPreview] = useState(false);
 	return (
 		<div className="App">
 			<div>
 				<did>
-					<TextEditer />
+					<TextEditer
+						email={email}
+						setEmail={setEmail}
+						greeting={importedGreeting}
+						setGreeting={setImportedGreeting}
+						body={importedBody}
+						setBody={setImportedBody}
+						closing={importedClosing}
+						setClosing={setImportedClosing}
+						setPreview={setPreview}
+						setCustomize={setCustomize}
+					/>
 				</did>
 			</div>
 		</div>
--- src/Components/TextEditer.js
+++ src/Components/TextEditer.js
@@ -5,7 +5,40 @@ import { Card } from "react-bootstrap";
 import logo from "../assets/newYear.jpg";
 import { EditText, EditTextarea } from "react-edit-text";
 
-function TextEditer() {
+function TextEditer(
+	email,
+	setEmail,
+	greeting,
+	setGreeting,
+	body,
+	setBody,
+	closing,
+	setClosing,
+	setPreview,
+	setCustomize
+) {
+	const sendEcard = () => {
+		let mailto = `mailto:${email}`;
+		mailto += "?subject=E-Card";
+		mailto += "&body=You have received an e-greeting card.";
+		mailto += "%0D%0A%0D%0A";
+		mailto += "Go To Greeting Card";
+		mailto += "%0D%0A";
+
+		let url = "http://Hiwot-Beshe.github.io/ecard/";
+		url += `?greeting=${encodeURIComponent(greeting)}`;
+		url += `&body=${encodeURIComponent(body)}`;
+		url += `&closing=${encodeURIComponent(closing)}`;
+
+		mailto += encodeURIComponent(url);
+
+		window.open(mailto);
+	};
+	const onSubmit = (event) => {
+		event.preventDefault();
+		setPreview(true);
+		setCustomize(false);
+	};
 	return (
 		<div className="App-header">
 			<Card className="App-card">
@@ -28,31 +61,50 @@ function TextEditer() {
 					<Card.Text>To customize your message, click the text</Card.Text>
 					<Card.Text>
 						<EditText
-							
 							style={{
 								padding: "10px",
 								fontSize: "24px",
 								color: "#FFF",
 								backgroundColor: "#000",
 							}}
-							defaultValue="Dear Mom"
+							value={greeting}
+							onChange={(event) => setGreeting(event.target.value)}
+							placeholder="Greeting"
+						/>
+					</Card.Text>
+					<Card.Text>
+						<EditText
+							style={{
+								padding: "10px",
+								fontSize: "24px",
+								color: "#FFF",
+								backgroundColor: "#000",
+							}}
+							value={greeting}
+							onChange={(event) => setGreeting(event.target.value)}
+							placeholder="Greeting"
 						/>
 					</Card.Text>
 					<Card.Text>
 						<br />
-						<EditTextarea placeholder="It’s the most wonderful time of the year…and you’re the most wonderful mom." />
+						<EditTextarea
+							value={body}
+							onChange={(event) => setBody(event.target.value)}
+							placeholder="It’s the most wonderful time of the year…and you’re the most wonderful mom."
+						/>
 					</Card.Text>
 					<Card.Text>
 						<br />
 						<EditText
-							
 							style={{
 								padding: "15px",
 								fontSize: "24px",
 								color: "#FFF",
 								backgroundColor: "#000",
 							}}
-							defaultValue=" Yours Hiwot"
+							value={closing}
+							onChange={(event) => setClosing(event.target.value)}
+							placeholder="Closing"
 						/>
 					</Card.Text>
 				</Card.Body>
@@ -60,6 +112,7 @@ function TextEditer() {
 
 			<div>
 				<EmailShareButton
+					onClick={sendEcard}
 					subject="e-card"
 					body={"This is my Holiday card for you."}
 					url="http://Hiwot-Beshe.github.io/ecard/"
