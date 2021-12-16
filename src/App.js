29c081601fd02568e2fa4e764d476dff4c428b37
Hiwot-Beshe <beshehiwot@gmail.com>
2021-12-15 20:53:53 -0800

    feat(add params)add page and share the message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
 import "./App.css";
-import TextEditer from "./Components/TextEditer";
+import GreetingCard from "./Components/GreetingCard";
+import CustomizeMessage from "./Components/CustomizedMessage";
 import { useState } from "react";
+
 
 function App() {
 	const query = new URLSearchParams(window.location.search);
@@ -10,30 +11,42 @@ function App() {
 		query.get("greeting") || "Dear Mom,"
 	);
 	const [importedBody, setImportedBody] = useState(
-		query.get("body") || "Have a comforting and joyous holiday!"
+		query.get("body") ||
+			"You’ll always be the heart of our holidays. Love you, Mom!"
 	);
 	const [importedClosing, setImportedClosing] = useState(
-		query.get("closing") || "Love, your son Juan"
+		query.get("closing") || "Love, Yours Hiwot"
 	);
-	
+	const [customize, setCustomize] = useState(false);
+	const [preview, setPreview] = useState(false);
+
 	return (
-		<div className="App">
-			<div>
-				<did>
-					<TextEditer
-						email={email}
-						setEmail={setEmail}
-						greeting={importedGreeting}
-						setGreeting={setImportedGreeting}
-						body={importedBody}
-						setBody={setImportedBody}
-						closing={importedClosing}
-						setClosing={setImportedClosing}
-						
-					/>
-				</did>
-			</div>
-		</div>
+		<>
+			{customize ? (
+				<CustomizeMessage
+					email={email}
+					setEmail={setEmail}
+					greeting={importedGreeting}
+					setGreeting={setImportedGreeting}
+					body={importedBody}
+					setBody={setImportedBody}
+					closing={importedClosing}
+					setClosing={setImportedClosing}
+					setPreview={setPreview}
+					setCustomize={setCustomize}
+				/>
+			) : (
+				<GreetingCard
+					email={email}
+					greeting={importedGreeting}
+					body={importedBody}
+					closing={importedClosing}
+					setCustomize={setCustomize}
+					preview={preview}
+					setPreview={setPreview}
+				/>
+			)}
+		</>
 	);
 }
 
